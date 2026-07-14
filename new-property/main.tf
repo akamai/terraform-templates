d9c82267538139f8962a4492179440e3a9ed87d0
Rafael Lopez <rlopezso@akamai.com>
2026-07-14 17:10:39 -0600

    fix: set proper module source ref

--- new-property/main.tf
+++ new-property/main.tf
@@ -93,7 +93,7 @@
 
 
 module "property" {
-  source = "git::ssh://git@github.com/akamai/terraform-templates-modules.git//delivery?ref=integration"
+  source = "git::ssh://git@github.com/akamai/terraform-templates-modules.git//delivery?ref=v1.5.0"
 
   contract_id = var.contract_id
   group_id    = var.group_id
